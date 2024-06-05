122d3f7e4878d87bd4fdf462a76d54cf8d771903
미뿌감 <80188808+20hyukim@users.noreply.github.com>
2024-06-05 15:16:36 +0900

    feat: @Transactional added

--- src/main/java/com/sparta/webfluxchat/service/ChatRoomService.java
+++ src/main/java/com/sparta/webfluxchat/service/ChatRoomService.java
@@ -1,12 +1,17 @@
 package com.sparta.webfluxchat.service;
 
 import com.sparta.webfluxchat.entity.ChatRoom;
+import com.sparta.webfluxchat.entity.ChatRoomUser;
 import com.sparta.webfluxchat.repository.ChatRoomRepository;
+import com.sparta.webfluxchat.security.UserDetailsImpl;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
-import org.apache.coyote.Response;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -15,6 +20,9 @@ import java.util.Optional;
 public class ChatRoomService {
 
     private final ChatRoomRepository chatRoomRepository;
+    private static final Logger logger = LoggerFactory.getLogger(ChatRoomService.class);
+
+    @Transactional
     public ResponseEntity<String> createChatRoom(String chatName) {
         Optional<ChatRoom> existingChatRoom = chatRoomRepository.findByName(chatName);
         if (existingChatRoom.isPresent()) {
@@ -26,7 +34,18 @@ public class ChatRoomService {
         return ResponseEntity.ok("성공적으로 채팅방이 생성되었습니다.");
     }
 
-    public List<ChatRoom> chatLists() {
+    @Transactional
+    public List<ChatRoom> chatLists(UserDetailsImpl userDetails) {
+        /*logger.info("chatLists function started");
+        List<ChatRoomUser> chatRoomUsers = userDetails.getUser().getChatRoomUsers();
+
+        List<ChatRoom> chatRooms = new ArrayList<>();
+
+        for (ChatRoomUser chatRoomUser : chatRoomUsers) {
+            logger.info(String.valueOf(chatRoomUser.getId()));
+            chatRooms.add(chatRoomUser.getChatRoom());
+        }*/
+        // 친구 entity 구현하고 나서 해야할 듯.
         return chatRoomRepository.findAll();
     }
 }
