4301cc47202d481adedfee715db931713d190036
미뿌감 <80188808+20hyukim@users.noreply.github.com>
2024-06-10 14:09:02 +0900

    feat: repository annotation added

--- src/main/java/com/sparta/webfluxchat/repository/ChatRoomRepository.java
+++ src/main/java/com/sparta/webfluxchat/repository/ChatRoomRepository.java
@@ -2,9 +2,11 @@ package com.sparta.webfluxchat.repository;
 
 import com.sparta.webfluxchat.entity.ChatRoom;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface ChatRoomRepository extends JpaRepository<ChatRoom, Long> {
     Optional<ChatRoom> findByName(String chatName);
 }
