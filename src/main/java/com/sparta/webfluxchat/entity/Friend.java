b4716164c38ea3018922c1e34f35e68b2e6e59c3
미뿌감 <80188808+20hyukim@users.noreply.github.com>
2024-06-10 17:23:36 +0900

    feat: friend entity created

---
+++ src/main/java/com/sparta/webfluxchat/entity/Friend.java
@@ -0,0 +1,11 @@
+package com.sparta.webfluxchat.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+
+
+public class Friend {
+
+}
