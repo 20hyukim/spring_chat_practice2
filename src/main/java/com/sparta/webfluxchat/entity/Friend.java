7390089c5b675c4e714a09c0289fc62addc45fae
미뿌감 <80188808+20hyukim@users.noreply.github.com>
2024-06-11 16:42:52 +0900

    feat: friend entity written

--- src/main/java/com/sparta/webfluxchat/entity/Friend.java
+++ src/main/java/com/sparta/webfluxchat/entity/Friend.java
@@ -1,11 +1,31 @@
 package com.sparta.webfluxchat.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.OneToOne;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
-
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@Table(name="friend")
 public class Friend {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private Long friendId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name="user_id")
+    private User user;
+
+
+    public Friend(User user, Long friendId) {
+        this.user= user;
+        this.friendId = friendId;
+    }
 }
