cfa8b7eb018bed9cbac34abd17dc08eb3f22315a
미뿌감 <80188808+20hyukim@users.noreply.github.com>
2024-06-11 16:45:14 +0900

    feat: added friend List to the model. So that, we can print out friend Lists in my Page

--- src/main/java/com/sparta/webfluxchat/controller/api/MyPageController.java
+++ src/main/java/com/sparta/webfluxchat/controller/api/MyPageController.java
@@ -1,5 +1,7 @@
 package com.sparta.webfluxchat.controller.api;
 
+import com.sparta.webfluxchat.dto.FriendDto;
+import com.sparta.webfluxchat.entity.User;
 import com.sparta.webfluxchat.security.UserDetailsImpl;
 import com.sparta.webfluxchat.service.MyPageService;
 import lombok.RequiredArgsConstructor;
@@ -8,10 +10,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
@@ -22,6 +26,8 @@ public class MyPageController {
     public String myPage(Model model, @AuthenticationPrincipal UserDetailsImpl userDetails){
         model.addAttribute("username", userDetails.getUser().getUsername());
         model.addAttribute("image", userDetails.getUser().getImageUrl());
+        List<FriendDto> friendList = myPageService.getFriendList(userDetails.getUser().getId());
+        model.addAttribute("friends", friendList);
         return "page";
     }
 
@@ -30,4 +36,11 @@ public class MyPageController {
         myPageService.setImage(image, userDetails.getUser().getId());
         return "redirect:/page";
     }
+
+    @PostMapping("/user/friend")
+    public String myFriend(@RequestParam Long friendId, @AuthenticationPrincipal UserDetailsImpl userDetails) {
+        myPageService.setFriend(friendId, userDetails.getUser());
+        return "redirect:/page";
+    }
+
 }
