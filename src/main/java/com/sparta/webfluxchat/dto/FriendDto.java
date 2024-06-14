9eb3cc91c5c4c0c27350bfca8e6f31c61e6e689b
미뿌감 <80188808+20hyukim@users.noreply.github.com>
2024-06-14 15:11:55 +0900

    feat: to transfer original name of friend

--- src/main/java/com/sparta/webfluxchat/dto/FriendDto.java
+++ src/main/java/com/sparta/webfluxchat/dto/FriendDto.java
@@ -8,11 +8,13 @@ import lombok.Setter;
 public class FriendDto {
     private Long id;
     private String name;
+    private String originalName;
     private String imageUrl;
 
-    public FriendDto(Long id, String name, String imageUrl) {
+    public FriendDto(Long id, String name, String originalName, String imageUrl) {
         this.id = id;
         this.name = name;
+        this.originalName = originalName;
         this.imageUrl = imageUrl;
     }
 }
