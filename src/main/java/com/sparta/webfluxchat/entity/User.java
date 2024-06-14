04af88620ccb932275676a002afa8f3979d55d3a
미뿌감 <80188808+20hyukim@users.noreply.github.com>
2024-06-14 10:54:06 +0900

    feat: added name column User entity

--- src/main/java/com/sparta/webfluxchat/entity/User.java
+++ src/main/java/com/sparta/webfluxchat/entity/User.java
@@ -30,6 +30,9 @@ public class User {
     private String email;
 
     @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
     @Enumerated(value = EnumType.STRING) //enum type을 db에 저장하고자 할 떄 // Enum type이 만약 USER라고 할 때, USER 그대로 db에 저장되는
     private UserRoleEnum role;
 
@@ -44,10 +47,11 @@ public class User {
     private List<Friend> friends;
 
 
-    public User(String username, String password, String email, UserRoleEnum role) {
+    public User(String username, String password, String email, String name, UserRoleEnum role) {
         this.username = username;
         this.password = password;
         this.email = email;
+        this.name = name;
         this.role = role;
     }
 
