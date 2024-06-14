b2529b07281e0b165b261933313fe480ed667eed
미뿌감 <80188808+20hyukim@users.noreply.github.com>
2024-06-14 14:49:45 +0900

    feat: dto to transfer friendId and friendName

---
+++ src/main/java/com/sparta/webfluxchat/dto/PageFriendRequestDto.java
@@ -0,0 +1,11 @@
+package com.sparta.webfluxchat.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class PageFriendRequestDto {
+    private Long friendId;
+    private String friendName;
+}
