feb8fb5d4caa03f88af850de47a333d2939264a8
미뿌감 <80188808+20hyukim@users.noreply.github.com>
2024-06-20 16:35:00 +0900

    feat: constructor added for ChatRoomUser

--- src/main/java/com/sparta/webfluxchat/entity/ChatRoomUser.java
+++ src/main/java/com/sparta/webfluxchat/entity/ChatRoomUser.java
@@ -2,11 +2,13 @@ package com.sparta.webfluxchat.entity;
 
 import jakarta.persistence.*;
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 
 @Entity
 @Getter
 @Setter
+@RequiredArgsConstructor
 public class ChatRoomUser {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -21,4 +23,8 @@ public class ChatRoomUser {
     private User user;
 
 
+    public ChatRoomUser(User user, ChatRoom chatRoom) {
+        this.chatRoom = chatRoom;
+        this.user = user;
+    }
 }
