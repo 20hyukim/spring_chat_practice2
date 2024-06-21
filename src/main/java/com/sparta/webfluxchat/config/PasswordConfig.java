c3f8537e4f92fec6a1c359519d9f4ce85cbeb6bb
미뿌감 <80188808+20hyukim@users.noreply.github.com>
2024-06-21 11:17:44 +0900

    refactor: deleted enter

--- src/main/java/com/sparta/webfluxchat/config/PasswordConfig.java
+++ src/main/java/com/sparta/webfluxchat/config/PasswordConfig.java
@@ -7,7 +7,6 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class PasswordConfig {//수동 등록할 Bean을 넣기 위해 만듦. passwordConfig
-
     @Bean//Bean으로 등록하고자 함. 이게 등록되어 있는 class위에 configuration을 넣어준다.
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
