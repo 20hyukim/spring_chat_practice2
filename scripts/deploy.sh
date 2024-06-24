03efc3004c5ac7c8aacc709b5a8f64217816b7a0
미뿌감 <80188808+20hyukim@users.noreply.github.com>
2024-06-24 13:23:18 +0900

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -9,14 +9,26 @@ JAR_PATH=$REPOSITORY/build/libs/$JAR_NAME
 
 CURRENT_PID=$(pgrep -f $APP_NAME)
 
-if [ -z $CURRENT_PID ]
-then
+if [ -z $CURRENT_PID ]; then
   echo "> 종료할 애플리케이션이 없습니다."
 else
-  echo "> kill -9 $CURRENT_PID"
+  echo "> kill -15 $CURRENT_PID"
   kill -15 $CURRENT_PID
   sleep 5
 fi
 
-echo "> Deploy - $JAR_PATH "
-nohup java -jar $JAR_PATH > /dev/null 2> /dev/null < /dev/null &
+echo "> Deploy - $JAR_PATH"
+nohup java -jar $JAR_PATH > $REPOSITORY/nohup.out 2>&1 &
+
+echo "> Deployment script completed."
+
+sleep 10
+
+NEW_PID=$(pgrep -f $APP_NAME)
+
+if [ -z $NEW_PID ]; then
+  echo "> 애플리케이션 시작 실패"
+  exit 1
+else
+  echo "> 애플리케이션이 성공적으로 시작되었습니다. PID: $NEW_PID"
+fi
