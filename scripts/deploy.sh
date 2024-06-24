398b59b1923bea9404c0e9a81427fdf44a80a810
미뿌감 <80188808+20hyukim@users.noreply.github.com>
2024-06-24 15:06:36 +0900

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,21 +3,21 @@
 REPOSITORY=/home/ubuntu/chat
 cd $REPOSITORY
 
-APP_NAME=chat
+APP_NAME=webflux-chat
 JAR_NAME=$(ls $REPOSITORY/build/libs/ | grep 'SNAPSHOT.jar' | tail -n 1)
 JAR_PATH=$REPOSITORY/build/libs/$JAR_NAME
 
 CURRENT_PID=$(pgrep -f $APP_NAME)
 
 if [ -z $CURRENT_PID ]; then
-  echo "> 종료할 애플리케이션이 없습니다."
+  echo "> No application running, skipping termination."
 else
-  echo "> kill -15 $CURRENT_PID"
+  echo "> Killing process $CURRENT_PID"
   kill -15 $CURRENT_PID
   sleep 5
 fi
 
-echo "> Deploy - $JAR_PATH"
+echo "> Deploying $JAR_PATH"
 nohup java -jar $JAR_PATH > $REPOSITORY/nohup.out 2>&1 &
 
 echo "> Deployment script completed."
@@ -27,8 +27,8 @@ sleep 10
 NEW_PID=$(pgrep -f $APP_NAME)
 
 if [ -z $NEW_PID ]; then
-  echo "> 애플리케이션 시작 실패"
+  echo "> Application failed to start"
   exit 1
 else
-  echo "> 애플리케이션이 성공적으로 시작되었습니다. PID: $NEW_PID"
+  echo "> Application started successfully. PID: $NEW_PID"
 fi
